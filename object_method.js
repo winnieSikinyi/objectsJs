966c86a6d4f59bc8f80f0b1f6925d4695491ea3b
winnieSikinyi <winniesikinyi@gmail.com>
2023-04-25 07:16:04 +0300

    first commit

---
+++ object_method.js
@@ -0,0 +1,71 @@
+// Given an array of objects, each object representing a person with a name
+//  and age property, write a function that returns a new array containing the 
+//  names of all people who are 18 years old or older.
+function person(persons){
+    persons = people
+  const people = [
+      { name: 'Alice', age: 17 },
+      { name: 'Eunice', age: 22 },
+      { name: 'Charlie', age: 14 },
+      { name: 'Max', age: 19 },
+    ];
+    let persons = new Object()
+    for(i=0;i>=people.length;i++){
+
+    }
+    return new Object
+  }
+  console.log(persons)
+  person(persons)
+  
+  //2. Write a function that takes an array of objects, where each object represents a
+  //  product with a name, price, and category property. The function should return 
+  // an object that groups the products by their categories, with the category names
+  // as keys and the arrays of products as values.
+  function array(){
+  const products = [
+    { name: 'Laptop', price: 1200, category: 'Electronics' },
+    { name: 'Shirt', price: 25, category: 'Clothing' },
+    { name: 'Headphones', price: 80, category: 'Electronics' },
+    { name: 'Shoes', price: 60, category: 'Clothing' },
+  ];
+  
+  
+  }
+  
+  //3.Given an array of objects, where each object represents a student with a name and 
+  // an array of scores, write a function that returns a new array containing the names 
+  // of all students whose average score is greater than or equal to 85.
+  function avrScore(avrStudent){
+    avrStudent.scores =new Object 
+  const students = [
+    { name: 'John', scores: [90, 80, 85] },
+    { name: 'Jane', scores: [95, 92, 88] },
+    { name: 'Jim', scores: [70, 80, 75] },
+    { name: 'Jill', scores: [85, 90, 84] },
+  ];
+  console.log(students)
+  students.values()
+  }
+  avrScore(avrStudent)
+  //4. Given an object representing a car, with properties for the make, 
+  // model, year, and a method to display the car's information, write a 
+  // function that takes the car object and adds a new method to the object called age.
+  //  The age method should return the current age of the car based on the current year 
+  //  and the car's year property.
+  function carObjects(theObjects){
+    theObjects.year= 1998
+  const car = {
+  make: 'Ford',
+  model: 'Ranger',
+  year: 2023,
+  displayInfo: function() {
+  
+    console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+  },
+  };
+  console.log(car.year)
+  console.log(theObjects)
+  }
+  
+  carObjects(theObjects)
